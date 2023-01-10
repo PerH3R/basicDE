5b75dc692d05176758b63cfa6ffa3de5183ca852
PerH3R <per.hermanus@gmail.com>
2023-01-10 15:34:06 +0100

    change random

--- src/agent.cc
+++ src/agent.cc
@@ -1,7 +1,7 @@
 #include "../include/agent.h"
 
 Agent::Agent(size_t dimension, Mutation* mutation_operator, Crossover* crossover_operator, Function* fitness_function) {
-	this->d = dimension;
+	this->dim = dimension;
 	this->mutation_operator = mutation_operator;
 	this->crossover_operator = crossover_operator;
 	this->fitness_function = fitness_function;
@@ -10,14 +10,16 @@ Agent::Agent(size_t dimension, Mutation* mutation_operator, Crossover* crossover
 
 	//TODO:initialize pos and donor
 	std::pair<double,double> range = fitness_function->get_range();
-	position.reserve(dimension);
-	donor.reserve(dimension);
-	for (int i = 0; i < d; ++i)	{
-
-		float value = static_cast <float> (rand()) / static_cast <float> (RAND_MAX/(range.first-range.second));
+	std::default_random_engine generator;
+	std::uniform_real_distribution<float> distribution(range.first, range.second);
+	position.reserve(dim);
+	donor.reserve(dim);
+	for (size_t i = 0; i < dim; ++i)	{
+		float value = distribution(generator);
 		this->position[i] = value;
 		this->donor[i] = value;
 	}
+	this->calculate_fitness();
 	// std::cout << "new agent at:" << position << std::endl;
 
 }
@@ -31,11 +33,11 @@ void Agent::mutate(std::vector<std::shared_ptr<Agent>> cur_gen, size_t idx){
 }
 
 void Agent::crossover(std::vector<std::shared_ptr<Agent>> next_gen, size_t idx){
-	next_gen[idx]->set_position(this->crossover_operator->apply(this->position, donor));
+	next_gen[idx]->set_position( this->crossover_operator->apply(position, donor) );
 }
 
 void Agent::calculate_fitness() {
-	this->fitness = this->fitness_function->evaluate(this->position);
+	this->fitness = this->fitness_function->evaluate(position);
 	fitness_uptodate = true;
 }
 
@@ -46,13 +48,13 @@ double Agent::get_fitness() {
 	return this->fitness;
 }
 
-std::vector<double> Agent::get_position() {
-	return this->position;
+std::vector<double>& Agent::get_position() {
+	return position;
 }
 
 void Agent::set_position(std::vector<double> new_position) {
 	//if dimension fits and new position is different from current position
-	if (new_position.size() == this->d && new_position != this->position) {
+	if (new_position.size() == this->dim && new_position != this->position) {
 		this->position = new_position;
 		fitness_uptodate = false;
 	}
