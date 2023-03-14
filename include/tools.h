edb72701de7a8a7b9b73d4903ddf59cb4e9c8f6e
PerH3R <per.hermanus@gmail.com>
2023-03-14 15:20:44 +0100

    rework Tools and add random functions

--- include/tools.h
+++ include/tools.h
@@ -6,8 +6,33 @@
 #include <random>
 #include "time.h"
 
-void set_seed(int seed=-1);
+//TODO
+#include<iostream>
 
-double get_euclidean_distance(std::vector<double> pos1,
-							  std::vector<double> pos2);
+class Tools{
+public:
+	Tools(){ 
+		auto current_time = time(NULL);
+		std::srand(current_time);
+	};
 
+	void set_seed(int seed=-1);
+
+	template <typename T>
+	double get_euclidean_distance(std::vector<T> pos1,
+								  std::vector<T> pos2);
+
+	double rand_double_unif(double const begin, double const end);
+
+	int rand_int_unif(int const start, int const end);
+			
+	double rand_double_norm(double const mean, double const stdDev);
+
+
+private:
+	std::mt19937 rng;
+	std::default_random_engine dev;
+
+};
+
+extern Tools tools;
--- src/tools.cc
+++ src/tools.cc
@@ -1,9 +1,35 @@
 #include "../include/tools.h"
 
-void set_seed(int seed){
+void Tools::set_seed(int seed){
 	if(seed==-1){
-		std::srand(time(NULL));
+		auto current_time = time(NULL);
+		rng.seed(current_time);
 	}else{
-		std::srand(seed);
+		rng.seed(seed);
 	}
 }
+
+template <typename T>
+double Tools::get_euclidean_distance(std::vector<T> pos1,
+							  std::vector<T> pos2){
+	//TODO
+
+	return -1.0;
+}
+
+double Tools::rand_double_unif(double const begin, double const end){
+	std::uniform_real_distribution<double> dist(begin, end);
+	return dist(rng);
+}
+
+int Tools::rand_int_unif(int const begin, int const end){
+	std::uniform_int_distribution<int> dist(begin, end-1);
+	return dist(rng);
+}
+			
+double Tools::rand_double_norm(double const mean, double const stdDev){
+	std::normal_distribution<double> N(mean, stdDev);
+	return N(rng);
+}
+
+Tools tools;
