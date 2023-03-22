4db80707fb017c0009039f27338a11fa8a803e09
PerH3R <per.hermanus@gmail.com>
2023-03-22 16:33:13 +0100

    add exponential crossover

--- include/crossover.h
+++ include/crossover.h
@@ -2,13 +2,15 @@
 
 // #include "agent.h"
 #include <random>
+#include "tools.h"
 
 //TODO: remove
 #include <iostream>
 // class Agent;
 
 enum CROSSOVER {
-	BINOMIAL
+	BINOMIAL,
+    EXPONENTIAL
 };
 
 class Crossover {
@@ -28,6 +30,15 @@ class Binomial : public Crossover {
 public:
     Binomial(size_t dim, float Cr = 0.2);// = default();
     ~Binomial(){};
+    CROSSOVER get_type();
+    std::vector<double> apply(std::vector<double> cur_pos, std::vector<double> donor_vec);
+};
+
+
+class Exponential : public Crossover {
+public:
+    Exponential(size_t dim, float Cr = 0.2);// = default();
+    ~Exponential(){};
     CROSSOVER get_type();
     std::vector<double> apply(std::vector<double> cur_pos, std::vector<double> donor_vec);
 };
--- src/crossover.cc
+++ src/crossover.cc
@@ -13,7 +13,7 @@ Crossover::Crossover(size_t dim, float Cr) : dim(dim), Cr(Cr){
 
 // }
 
-// Randdiv1
+// Binomial
 Binomial::Binomial(size_t dim, float Cr) : Crossover(dim, Cr){
 	// this->dim = dim;
 	// this->Cr = Cr;
@@ -61,5 +61,56 @@ std::vector<double> Binomial::apply(std::vector<double> cur_pos, std::vector<dou
 	return new_pos;
 }
 
+// Exponential
+Exponential::Exponential(size_t dim, float Cr) : Crossover(dim, Cr){
+	// this->dim = dim;
+	// this->Cr = Cr;
+}
 
+
+CROSSOVER Exponential::get_type() {
+  return BINOMIAL;
+}
+
+// Apply crossover on all agents, returns selection
+std::vector<double> Exponential::apply(std::vector<double> cur_pos, std::vector<double> donor_vec){
+	// std::cout << "co apply dim: " << this->dim << std::endl;
+
+	std::vector<double> new_pos = cur_pos;
+	new_pos.reserve(this->dim);
+
+	//TODO:sanity check, are cur_pos and donor vec same dim as 'n'
+	if (cur_pos.size() != this->dim){
+		std::cerr << "co apply invalid posiiton size\n";
+	}
+	if (donor_vec.size() != this->dim){
+		std::cerr << "co apply invalid donor size\n";
+	}
+
+	int crossover_point = tools.rand_int_unif(0,this->dim);
+	new_pos[crossover_point] = donor_vec[crossover_point];
+	int current_index = crossover_point+1;
+	// bool returned = false;
+
+	while (current_index % this->dim < crossover_point){
+		if (tools.rand_double_unif(0.0,1.0) < this->Cr){
+			new_pos[current_index] = donor_vec[current_index];
+		}else{
+			break;
+		}
+		current_index++;
+	}
+
+
+	if (cur_pos.size() != this->dim){
+		std::cerr << "co apply invalid posiiton size\n";
+	}
+	if (donor_vec.size() != this->dim){
+		std::cerr << "co apply invalid donor size\n";
+	}if (new_pos.size() != this->dim){
+		std::cerr << "co apply invalid posiiton size\n";
+	}
+	// std::cout << "co apply pos: " << new_pos.size() << " dim: " << this->dim << std::endl;
+	return new_pos;
+}
 
