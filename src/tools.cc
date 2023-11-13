5ce1dc9fcfeb0895c4faad1011cd93c027651970
PerH3R <per.hermanus@gmail.com>
2023-11-13 15:58:06 +0100

    consts and return fix

--- src/tools.cc
+++ src/tools.cc
@@ -10,29 +10,29 @@ void Tools::set_seed(int seed){
 }
 
 
-double Tools::rand_double_unif(double const begin, double const end){
+double Tools::rand_double_unif(double const& begin, double const& end){
 	std::uniform_real_distribution<double> dist(begin, end);
 	return dist(rng);
 }
 
-int Tools::rand_int_unif(int const begin, int const end){
+int Tools::rand_int_unif(int const& begin, int const& end){
 	std::uniform_int_distribution<int> dist(begin, end-1); // for [begin, end) dist
 	return dist(rng);
 }
 			
-double Tools::rand_double_norm(double const mean, double const stdDev){
+double Tools::rand_double_norm(double const& mean, double const& stdDev){
 	std::normal_distribution<double> N(mean, stdDev);
 	return N(rng);
 }
 
-double Tools::rand_mantegna(double const sig_u, double const sig_v, double const alpha){
+double Tools::rand_mantegna(double const& sig_u, double const& sig_v, double const& alpha){
 	double u_dist = this->rand_double_unif(0, sig_u);
 	double v_dist = this->rand_double_unif(0, sig_v);
 
 	return u_dist / std::pow(std::abs(v_dist), (1/alpha));
 }
 
-std::vector<double> Tools::vec_sub(std::vector<double> first, std::vector<double> second){
+std::vector<double> Tools::vec_sub(std::vector<double> const& first, std::vector<double> const& second){
 	if (first.size() == second.size()){
 		std::vector<double> result;
 		for (int i = 0; i < first.size(); ++i){
@@ -45,7 +45,7 @@ std::vector<double> Tools::vec_sub(std::vector<double> first, std::vector<double
 	}
 }
 
-std::vector<double> Tools::vec_scale(std::vector<double> vec, double scalar){
+std::vector<double> Tools::vec_scale(std::vector<double> const& vec, double const& scalar){
 	std::vector<double> result;
 	for (int i = 0; i < vec.size(); ++i){
 	 	result.push_back(vec[i]*scalar);
@@ -53,7 +53,7 @@ std::vector<double> Tools::vec_scale(std::vector<double> vec, double scalar){
 	return result; 
 }
 
-std::vector<double> Tools::vec_sum(std::vector<double> first, std::vector<double> second){
+std::vector<double> Tools::vec_sum(std::vector<double> const& first, std::vector<double> const& second){
 	if (first.size() == second.size()){
 		std::vector<double> result;
 		for (int i = 0; i < first.size(); ++i){
