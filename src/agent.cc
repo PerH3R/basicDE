346085f50b8ed923337b90261b77a04fb5ac7328
PerH3R <per.hermanus@gmail.com>
2023-12-07 15:16:11 +0100

    turn of not working auto F/Cr

--- src/agent.cc
+++ src/agent.cc
@@ -12,8 +12,10 @@ Agent::Agent(size_t dimension, std::shared_ptr<Mutation> mutation_operator, std:
 	this->donor.reserve(dim);
 
 
-	this->mutation_operator->auto_set_F(this->crossover_operator->get_type());
-	this->crossover_operator->set_Cr( this->mutation_operator->get_precalculated_Cr(this->crossover_operator->get_type()) );
+	auto Cr_type = this->crossover_operator->get_type();
+	// this->mutation_operator->auto_set_F(Cr_type);
+	auto auto_Cr = this->mutation_operator->get_precalculated_Cr(Cr_type);
+	// this->crossover_operator->set_Cr( auto_Cr );
 
 	//random initialization
 	for (size_t i = 0; i < dim; ++i){
