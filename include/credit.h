e45f22b1246db717e1161cdb421b10c3b438265d
PerH3R <per.hermanus@gmail.com>
2024-01-09 14:32:16 +0100

    add credit.h

---
+++ include/credit.h
@@ -0,0 +1,35 @@
+#include "tools.h"
+
+//Credit calculates improvement scores for individual agents
+
+class Credit{
+public:
+	Credit(int lp) : lp(lp) { };
+	virtual ~Credit() { };
+	virtual double get_credit(const std::vector< 
+								std::tuple<
+									std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>
+								>
+							> history,
+							std::vector<double> mean_position) = 0;
+protected:
+	int lp; //learning period
+};
+
+class FitnessImprovement : public Credit{
+public:
+	FitnessImprovement(int lp) : Credit(lp) {};
+	~FitnessImprovement() {};
+	double get_credit(const std::vector< 
+						std::tuple<
+							std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>
+						>
+					> history,
+					std::vector<double> mean_position){
+		const double last_fitness = std::get<1>(history[history.size() - 1]);
+		const double first_fitness = std::get<1>(history[history.size() - this->lp]); //why does rbegin iterator straight up not work :'(
+		double fitness_improvement = std::abs(first_fitness - last_fitness); //abs just in case someone want to maximise or smth
+		return fitness_improvement;
+	}
+};
+
