b9de077037017d600eae87166b36ddbce7cc12ce
PerH3R <per.hermanus@gmail.com>
2024-01-25 16:49:43 +0100

    shorten debug prints

--- src/adaptation.cc
+++ src/adaptation.cc
@@ -232,6 +232,7 @@ void MABManager::update_scores(){
 		for (int config = 0; config < operator_configurations.size(); config++){
 			if (agent_has_config(agent, operator_configurations[config])){
 				//config exists in database
+				std::cout << "f";
 				idx = config;
 				break;
 			}
@@ -251,7 +252,6 @@ void MABManager::update_scores(){
 	}
 
 	//apply scoring method and update
-	int _ = 0; //TODO
 	for(operator_configuration& config : this->operator_configurations){
 		if (!config.lp_improvements.empty()){
 			double score_avg = tools.vec_avg(config.lp_improvements);
@@ -259,11 +259,11 @@ void MABManager::update_scores(){
 			double new_Q = config.scores.back() * (1-this->alpha) + config.Q.back() * (1-this->alpha);
 			config.Q.push_back(new_Q);
 		}else{
-			std::cout <<  "lp_improvements empty" << std::endl;;
+			std::cout <<  "e";
 		}		
 		config.lp_improvements.clear();
 	}
-	_ = 0; //TODO
+	std::cout << std::endl;
 }
 
 void MABManager::add_config_from_agent(Agent* a){
