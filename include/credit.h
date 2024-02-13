5476bd0d6851f03861dcdec2d72124ce085e100b
PerH3R <per.hermanus@gmail.com>
2024-02-13 14:26:10 +0100

    add logh fitnessimprovement

--- include/credit.h
+++ include/credit.h
@@ -33,3 +33,20 @@ public:
 	}
 };
 
+class LogFitnessImprovement : public Credit{
+public:
+	LogFitnessImprovement(int lp) : Credit(lp) {};
+	~LogFitnessImprovement() {};
+	double get_credit(const std::vector< 
+						std::tuple<
+							std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>
+						>
+					> history,
+					std::vector<double> mean_position){
+		const double last_fitness = std::get<1>(history[history.size() - 1]);
+		const double first_fitness = std::get<1>(history[history.size() - this->lp]); //why does rbegin iterator straight up not work :'(
+		double fitness_improvement = std::abs(first_fitness - last_fitness); //abs just in case someone want to maximise or smth
+		return std::tanh(fitness_improvement); //TODO is log ok here
+	}
+};
+
