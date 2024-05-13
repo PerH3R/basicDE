d89d42a3c36645d727d478f076161d8192b167e3
PerH3R <per.hermanus@gmail.com>
2024-05-13 14:17:28 +0200

    update auc script

--- scripts/auc_analyzer.py
+++ scripts/auc_analyzer.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 
+
+#TODO: exclude keywords in avg_keyword_rank | selelct column to average (default auc-avg-rank)
+
+
 #import data
 dim5_df = pd.read_csv("./AUC_ECDFEAF_MULTI_DIM5.csv")
 dim20_df = pd.read_csv("./AUC_ECDFEAF_MULTI_DIM20.csv")
@@ -33,33 +37,46 @@ combined_df['AUC-AVG-RANK'] = combined_df[['AUC-5-RANK', 'AUC-20-RANK']].mean(ax
 
 print(combined_df)
 
-def avg_keyword_rank(keyword=""):
+def avg_keyword_rank(keyword="", banword="", col_name="AUC-AVG-RANK"):
 	count = 0
 	rank = 0
 	for index, row in combined_df.iterrows():
 		if row['ID'].find(keyword) > 0:
-			# print(row['ID'], "true")
-			count += 1
-			rank += row['AUC-AVG-RANK']
+			if not (row['ID'].find(banword) > 0 and banword==""):
+				# print(row['ID'], "true")
+				count += 1
+				rank += row[col_name]
 		# else:
 		# 	print(row['ID'], "false")
 	if count > 0:
 		return rank/count
 	return -1
 
-print('Bin:', str(avg_keyword_rank("Bin")))
-print('sel0:', str(avg_keyword_rank("sel0")))
-print('sel1:', str(avg_keyword_rank("sel1")))
-print("=========")
-print('crd0:', str(avg_keyword_rank("crd0")))
-print('crd1:', str(avg_keyword_rank("crd1")))
-print('crd2:', str(avg_keyword_rank("crd2")))
-print('crd3:', str(avg_keyword_rank("crd3")))
-print("=========")
-print('lp1:', str(avg_keyword_rank("lp1")))
-print('lp5:', str(avg_keyword_rank("lp5")))
-print('lp10:', str(avg_keyword_rank("lp10")))
-print('lp20:', str(avg_keyword_rank("lp20")))
-print('lp50:', str(avg_keyword_rank("lp50")))
-print('lp100:', str(avg_keyword_rank("lp100")))
-print('lp200:', str(avg_keyword_rank("lp200")))
+print("====ALL====")
+print("keyword\t\t dim5\t dim20\t dim5&20")
+print('_ (75): \t', 	f'{avg_keyword_rank("_", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("_", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("_"):.2f}')
+print("====NON-MAB====")
+print("keyword\t\t dim5\t dim20\t dim5&20")
+print('Bin (16): \t', 		f'{avg_keyword_rank("Bin", col_name="AUC-5-RANK"):.2f}', 		"\t", f'{avg_keyword_rank("Bin", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("Bin"):.2f}')
+print('Rand_search(1):\t', 	f'{avg_keyword_rank("search", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("search", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("search"):.2f}')
+print('Rand_vec (1): \t', 	f'{avg_keyword_rank("vectors", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("vectors", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("vectors"):.2f}')
+print('Rand_op (1): \t', 	f'{avg_keyword_rank("operators", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("operators", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("operators"):.2f}')
+print("====OPERATOR SELECTOR====")
+print("keyword\t\t dim5\t dim20\t dim5&20")
+print('sel0 (28): \t', 	f'{avg_keyword_rank("sel0", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("sel0", col_name="AUC-20-RANK"):.2f}', "\t",f'{avg_keyword_rank("sel0"):.2f}')
+print('sel1 (28): \t', 	f'{avg_keyword_rank("sel1", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("sel1", col_name="AUC-20-RANK"):.2f}', "\t",f'{avg_keyword_rank("sel1"):.2f}')
+print("====CREDIT ASSIGNER====")
+print("keyword\t\t dim5\t dim20\t dim5&20")
+print('crd0 (14): \t', 	f'{avg_keyword_rank("crd0", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("crd0", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("crd0"):.2f}')
+print('crd1 (14): \t', 	f'{avg_keyword_rank("crd1", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("crd1", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("crd1"):.2f}')
+print('crd2 (14): \t', 	f'{avg_keyword_rank("crd2", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("crd2", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("crd2"):.2f}')
+print('crd3 (14): \t', 	f'{avg_keyword_rank("crd3", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("crd3", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("crd3"):.2f}')
+print("====LEARNING PERIOD====")
+print("keyword\t\t dim5\t dim20\t dim5&20")
+print('lp1 (8): \t', 	f'{avg_keyword_rank("lp1", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("lp1", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp1"):.2f}')
+print('lp5 (8): \t', 	f'{avg_keyword_rank("lp5", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("lp5", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp5"):.2f}')
+print('lp10 (8): \t', 	f'{avg_keyword_rank("lp10", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("lp10", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp10"):.2f}')
+print('lp20 (8): \t', 	f'{avg_keyword_rank("lp20", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("lp20", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp20"):.2f}')
+print('lp50 (8): \t', 	f'{avg_keyword_rank("lp50", col_name="AUC-5-RANK"):.2f}', 	"\t", f'{avg_keyword_rank("lp50", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp50"):.2f}')
+print('lp100 (8): \t', 	f'{avg_keyword_rank("lp100", col_name="AUC-5-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp100", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp100"):.2f}')
+print('lp200 (8): \t', 	f'{avg_keyword_rank("lp200", col_name="AUC-5-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp200", col_name="AUC-20-RANK"):.2f}', "\t", f'{avg_keyword_rank("lp200"):.2f}')
