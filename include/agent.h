fd3e1ee7de5d8be03a292fac4b6c611c2562d735
Per <per.hermanus@gmail.com>
2024-07-05 14:19:42 +0200

    more comments - Agent

--- include/agent.h
+++ include/agent.h
@@ -23,58 +23,91 @@ public:
 			std::shared_ptr<Boundary> boundary_correction, ioh::problem::RealSingleObjective* target_function, unsigned int* budget, int resample_limit);
 	~Agent();
 
+	// Applies mutation operator to current Agent
 	void mutate(const std::vector<Agent*>& cur_gen, size_t idx);
+
+	// Applies mutation operator to current Agent
 	void crossover(std::vector<Agent*>& next_gen, size_t idx);
 
+	// Updates and returns fitness of current Agent
 	double get_fitness();
 
-	bool check_position_oob(const std::vector<double>& given_position);
-
+	// return current position
 	const std::vector<double>& get_position();
+
+	// Move current Agent to a new position
 	void set_position(std::vector<double> new_position);
 
+	// Checks if supplied position is within valid problem bounds. Called in mutate() (see resample_limit)
+	bool check_position_oob(const std::vector<double>& given_position);
 
+	// Returns currently set operators
 	CROSSOVER get_crossover();
 	MUTATION get_mutation();
 	BOUNDARY get_boundary();
-	int get_id() const {return id;};
 
+	// Returns identifier nr. of current Agent
+	const int get_id() const {return id;};
+
+	// Change set operator to a new operator using a pointer to to said operator.
 	void set_crossover(std::shared_ptr<Crossover> new_crossover){this->crossover_operator = new_crossover;};
 	void set_mutation(std::shared_ptr<Mutation> new_mutation){this->mutation_operator = new_mutation;};
 	void set_boundary(std::shared_ptr<Boundary> new_boundary){this->boundary_correction = new_boundary;};
 
+	// get a pointer to currently set operator
 	std::shared_ptr<Crossover> get_crossover_ptr(){return this->crossover_operator;};
 	std::shared_ptr<Mutation> get_mutation_ptr(){return this->mutation_operator;};
 	std::shared_ptr<Boundary> get_boundary_ptr(){return this->boundary_correction;};
 
+	// Add current state of Agent to history
 	std::tuple<std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>> update_history();
+
+	// Add a (constructed) state to history
 	void add_history(std::tuple<std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>> snapshot);
+
+	// Retrieve history of current Agent
 	const std::vector< std::tuple<std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>> >& get_history() {return this->history;};
+	
+	// Prints out entire history to terminal (printing of positions optional to increase readability)
 	void print_history(bool print_positions = false);
 
 
 private:
+	// Prints a position to terminal. Currently unused, but used for debug purposes
 	void print_position(std::vector<double> to_print);
+
+	// Calculates and updates fitness using the currently stored position
 	void calculate_fitness();
-	const int id;
-	
-	size_t dim;
-	std::vector<double> position;
-	std::vector<double> donor;
 	double fitness;
+
+	const int id;	
+	size_t dim;
 	
 
+	// Current position of the Agent. Base vector
+	std::vector<double> position;
+
+	// Donor vector calculated by mutation operator
+	std::vector<double> donor;
+	
+	// Currently used operators
 	std::shared_ptr<Mutation> mutation_operator;
 	std::shared_ptr<Crossover> crossover_operator;
 	std::shared_ptr<Boundary> boundary_correction;
 
+	// Pointer to problem function
 	ioh::problem::RealSingleObjective* target_function;
 	
+	// Total shared budget for entire population
 	unsigned int* budget;
+
+	// How often the mutation operator can be retried if the resulting donor vector is out of bounds
 	int resample_limit; //10+ln(dim)^2
 
+	// Keeps track of movement in order to not recalculate fitness if no movement has occured
 	bool fitness_uptodate;
 
+	// History of the agent
 	// position, fitness, crossover operator, mutation operator, boundary operator
 	std::vector<
 		std::tuple<
--- src/agent.cc
+++ src/agent.cc
@@ -11,12 +11,6 @@ Agent::Agent(int id, size_t dimension, std::shared_ptr<Mutation> mutation_operat
 	this->position.reserve(dim);
 	this->donor.reserve(dim);
 
-
-	// auto Cr_type = this->crossover_operator->get_type();
-	// // this->mutation_operator->auto_set_F(Cr_type);
-	// auto auto_Cr = this->mutation_operator->get_precalculated_Cr(Cr_type);
-	// this->crossover_operator->set_Cr( auto_Cr );
-
 	//random initialization
 	for (size_t i = 0; i < dim; ++i){
 		double value = tools.rand_double_unif(target_function->bounds().lb[i], target_function->bounds().ub[i]);
@@ -41,23 +35,22 @@ void Agent::print_position(std::vector<double> to_print){
 
 void Agent::mutate(const std::vector<Agent*>& cur_gen, size_t idx){	
 
-	//attempt generation of donor vector within bounds
+	// Attempt generation of donor vector within bounds
 	int tries = 0;
 	do{
 		tries++;
 		this->donor = this->mutation_operator->apply(cur_gen, idx);
-		// print_position(donor_vec);
 	}while((check_position_oob(donor) == true) && (tries <= this->resample_limit));
 	
-	//apply correction if no donor vector within bounds could be generated
+	// Apply correction if no donor vector within bounds could be generated
 	if (check_position_oob(donor) == true){
 		this->donor = boundary_correction->apply(this->donor);
 	}
 }
 
 void Agent::crossover(std::vector<Agent*>& next_gen, size_t idx){
+	// Obtain a new position given the current position and donor vector
 	std::vector<double> new_position = this->crossover_operator->apply(this->position, this->donor);
-
 	next_gen[idx]->set_position(new_position);
 }
 
@@ -70,6 +63,7 @@ void Agent::calculate_fitness() {
 
 
 double Agent::get_fitness() {
+	// If movement has occured, ask problem funtion fitness of new position at cost of budget
 	if (fitness_uptodate == false && *budget > 0) {
 		calculate_fitness();
 		*budget -= 1;
@@ -86,17 +80,18 @@ MUTATION Agent::get_mutation(){return this->mutation_operator->get_type();}
 BOUNDARY Agent::get_boundary(){return this->boundary_correction->get_type();}
 
 void Agent::set_position(std::vector<double> new_position) {
-	//if dimension fits
+	// Make sure dimension fits
 	if (new_position.size() != this->position.size()) {std::cerr << "incorrect dimensions new position\n";}
 
+	// New position should be in bounds, check if this is the case
 	std::vector<double> corrected_position = boundary_correction->apply(new_position);
 	if (corrected_position != new_position){
 		new_position = corrected_position;
-		std::cerr << "emergency correction applied - please investigate";
+		std::cerr << "correction applied - please investigate mutation or crossover operator";
 	}
 	
 
-	//update position
+	// Update position
 	for (size_t i = 0; i < this->dim; ++i){
 		if (this->position[i] != new_position[i]){
 			this->position[i] = new_position[i];
@@ -116,13 +111,12 @@ bool Agent::check_position_oob(const std::vector<double>& given_position){
 
 std::tuple<std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>> Agent::update_history(){
 	std::tuple<std::vector<double>, double, std::shared_ptr<Crossover>, std::shared_ptr<Mutation>, std::shared_ptr<Boundary>> t;
-	if (fitness_uptodate){
-		t = std::make_tuple(this->position, this->fitness, this->crossover_operator, this->mutation_operator, this->boundary_correction);
-		this->history.push_back(t);
-		
-	}else{
-		std::cerr << "Hey, the fitness isn't up to date. Why?" << std::endl;
+	if (!fitness_uptodate){		
+		this->calculate_fitness();
+		std::cerr << "Fitness wasn't up to date. Why?" << std::endl;
 	}	
+	t = std::make_tuple(this->position, this->fitness, this->crossover_operator, this->mutation_operator, this->boundary_correction);
+		this->history.push_back(t);
 	return t;
 }
 
@@ -135,14 +129,14 @@ void Agent::print_history(bool print_positions){
 		std::string hist_string= "";
 		const auto [pos, fitness, CrOpPtr, MutOpPtr, BoundOpPtr] = snapshot;
 		
-		// print position
+		// print position if asked to
 		if (print_positions){
 			for(double i : pos){
 			std::cout << i <<',';
 			}
 		}
 				
-		//print outher information
+		//print other information
 		std::cout << " " << fmt::format("{}", fitness) << " " << MutOpPtr->get_F() << " " << CrOpPtr->get_Cr() << " " 
 					<< CROSSOVER_NAMES[CrOpPtr->get_type()] << " " << MUTATION_NAMES[MutOpPtr->get_type()] << " " << BOUNDARY_NAMES[BoundOpPtr->get_type()] << std::endl;
 		
