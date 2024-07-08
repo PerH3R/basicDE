0b2ae9c5155ae4f87232f281edc2a3691f79d575
Per <per.hermanus@gmail.com>
2024-07-08 14:30:46 +0200

    more comments - Crossover (also remove probably unreachable warnings)

--- include/crossover.h
+++ include/crossover.h
@@ -19,6 +19,7 @@ public:
     virtual CROSSOVER get_type() = 0;
     virtual std::vector<double> apply(const std::vector<double>& cur_pos, const std::vector<double>& donor_vec) = 0;
 
+    // Allow for changing of crossover rate
     float get_Cr(){return this->Cr;}
     void set_Cr(float new_Cr){this->Cr = new_Cr;}
 
@@ -27,6 +28,8 @@ protected:
     float Cr;
 };
 
+
+// Binomial crossover - Each index is randomly picked based on Cr from the respective index of either the base vector or the donor vector
 class Binomial : public Crossover {
 public:
     Binomial(size_t dim, float Cr = 0.2) : Crossover(dim, Cr){ };// = default();
@@ -35,7 +38,8 @@ public:
     std::vector<double> apply(const std::vector<double>& cur_pos, const std::vector<double>& donor_vec) override;
 };
 
-
+// Expontential crossover - A random start index is chosen. That index and each subsequent are 
+// taken from donor vector until Prand > Cr happens. All other indexes are from base vector.
 class Exponential : public Crossover {
 public:
     Exponential(size_t dim, float Cr = 0.2) : Crossover(dim, Cr){ };// = default();
--- src/crossover.cc
+++ src/crossover.cc
@@ -11,33 +11,18 @@ std::vector<double> Binomial::apply(const std::vector<double>& cur_pos, const st
 
 	std::vector<double> new_pos;
 	new_pos.reserve(this->dim);
-
-	//TODO:sanity check, are cur_pos and donor vec same dim as 'n'
-	if (cur_pos.size() != this->dim){
-		std::cerr << "co apply invalid posiiton size\n";
-	}
-	if (donor_vec.size() != this->dim){
-		std::cerr << "co apply invalid donor size\n";
-	}
 	
+	// pick randomly from base or donor vector based on Cr
 	unsigned int crossover_point = tools.rand_uint_unif(0,this->dim);
 	for (size_t i = 0; i < this->dim; ++i){
 		float chance = tools.rand_double_unif(0.0, 1.0);
 		if ((chance <= Cr) || (i == crossover_point))		{
-			// std::cout << "donated position " << i << std::endl;
 			new_pos.push_back(donor_vec[i]);
 		}else{
 			new_pos.push_back(cur_pos[i]);
 		}
 	}
-	if (cur_pos.size() != this->dim){
-		std::cerr << "co apply invalid posiiton size\n";
-	}
-	if (donor_vec.size() != this->dim){
-		std::cerr << "co apply invalid donor size\n";
-	}if (new_pos.size() != this->dim){
-		std::cerr << "co apply invalid posiiton size\n";
-	}
+
 	return new_pos;
 }
 
@@ -48,15 +33,18 @@ CROSSOVER Exponential::get_type() {
 
 // Apply crossover on all agents, returns selection
 std::vector<double> Exponential::apply(const std::vector<double>& cur_pos, const std::vector<double>& donor_vec){
-
+	// initialize donor vector from base vector
 	std::vector<double> new_pos = cur_pos;
 	new_pos.reserve(this->dim);
 
+	// set crossover point
 	unsigned int crossover_point = tools.rand_uint_unif(0,this->dim);
+	// crossover point is always from donor
 	new_pos[crossover_point] = donor_vec[crossover_point];
 	unsigned int current_index = crossover_point+1;
-	// bool returned = false;
-
+	
+	// Replace indexes with donor vector until looped trough entire vector
+	// or until a chance based on Cr is reched
 	while (current_index % this->dim < crossover_point){
 		if (tools.rand_double_unif(0.0,1.0) <= this->Cr){
 			new_pos[current_index % this->dim] = donor_vec[current_index % this->dim];
